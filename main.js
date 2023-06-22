99aa05253d6921383f220deb09dc5af6efbada28
Marlene Pucher <99093092+Honeymoon12345@users.noreply.github.com>
2023-06-22 08:23:22 +0200

    init commit

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const gelesen = document.getElementById("gelesen");
 const iconDelete = document.getElementById("iconDelete");
 const iconRead = document.getElementById("iconRead");
 const btn = document.getElementById("btn");
+const bookShelf = document.querySelector(".bookShelf");
 
 let books = [];
 let id = 0;
@@ -14,50 +15,75 @@ function Book(titel, autor, seiten, gelesen) {
   this.titel = titel;
   this.autor = autor;
   this.seiten = seiten;
-  this.gelesen = gelesen;
+  if (gelesen) {
+    this.gelesen = "Read";
+  } else {
+    this.gelesen = "Unread";
+  }
   this.id = id++;
-  // this.showContent = function () {
-  //   this.books.array.forEach((element) => {});
-  // };
 }
 
 function addBookToLibary(event) {
-  if (gelesen.checked) {
-    console.log("test");
-    const book = new Book(titel.value, autor.value, seiten.value, "Read");
-  } else {
-    const book = new Book(titel.value, autor.value, seiten.value, "Unread");
-  }
+  const book = new Book(
+    titel.value,
+    autor.value,
+    seiten.value,
+    gelesen.checked
+  );
 
-  books.push(book);
-  document.querySelector(".bookShelf").innerHTML = "";
-  books.forEach((book) => {
-    let newBook = document.createElement("div");
-    newBook.innerHTML = `
-      <div class="container" data-id="${id}">
+  if (check()) {
+    books.push(book);
+    document.querySelector(".bookShelf").innerHTML = "";
+    books.forEach((book) => {
+      let newBook = document.createElement("div");
+      newBook.innerHTML = `
+      <div class="container" data-id="${book.id}">
         <p class="titel">${book.titel}</p>
         <p class="autor">${book.autor}</p>
         <p class="seiten">${book.seiten}</p>
-        <!-- <p class="lesestatus">${book.gelesen}</p> -->
-        <p class="lesestatus">Unread</p>
-        <button class="delete-book">
+        <p class="lesestatus">${book.gelesen}</p>
+        <button class="delete-book btn2">
             <span id="iconDelete" class="material-symbols-outlined iconDelete">
               delete
             </span>
           </button>
-          <button class="read-book">
+          <button class="read-book btn2">
             <span id="iconRead" class="material-symbols-outlined iconRead">
               local_library
             </span>
           </button>
       </div>`;
-    document.querySelector(".bookShelf").appendChild(newBook);
-  });
+      document.querySelector(".bookShelf").appendChild(newBook);
+    });
+  }
 }
 
-btn.addEventListener("click", addBookToLibary);
-
-const bookShelf = document.querySelector(".bookShelf");
+function check() {
+  if (titel.value == "" && autor.value == "" && seiten.value == "") {
+    alert("Please fill in all fields!");
+    return false;
+  } else if (titel.value == "" && autor.value == "") {
+    alert("Please add the Title and the Author!");
+    return false;
+  } else if (titel.value == "" && seiten.value == "") {
+    alert("Please add the Title and the Pages!");
+    return false;
+  } else if (seiten.value == "" && autor.value == "") {
+    alert("Please add the Author and the Pages!");
+    return false;
+  } else if (titel.value == "") {
+    alert("Please add the Title!");
+    return false;
+  } else if (autor.value == "") {
+    alert("Please add the Author!");
+    return false;
+  } else if (seiten.value == "") {
+    alert("Please add the Pages!");
+    return false;
+  } else {
+    return true;
+  }
+}
 
 const addEventBook = (rootElement, event) => {
   rootElement.addEventListener(
@@ -67,50 +93,30 @@ const addEventBook = (rootElement, event) => {
       while (targetElement != null) {
         if (targetElement.matches(".delete-book")) {
           console.log("löschen");
-          const btnDel = document.querySelector(".delete-book");
-
+          const btnDel = targetElement;
           let index = btnDel.parentElement.dataset.id;
-          btnDel.parentElement.remove();
-          books.splice(index - 1, 1);
           console.log(index);
+          btnDel.parentElement.remove();
+          books.splice(index, 1);
         }
-
-        targetElement = targetElement.parentElement;
-      }
-    },
-    true
-  );
-};
-
-addEventBook(bookShelf, "click");
-
-const readChange = (rootElement, event) => {
-  rootElement.addEventListener(
-    event,
-    (e) => {
-      let targetElement = e.target;
-      const btnRead = document.querySelector(".read-book");
-      let read = btnRead.parentElement.dataset.id;
-      while (targetElement != null) {
         if (targetElement.matches(".read-book")) {
-          if (document.querySelector(".lesestatus").innerHTML == "Read") {
-            document.querySelector(".lesestatus").innerHTML = "Unread";
+          const bookChange =
+            targetElement.parentElement.querySelector(".lesestatus");
+          let index = bookChange.parentElement.dataset.id;
+          if (bookChange.innerHTML == "Read") {
+            bookChange.innerHTML = "Unread";
+            books[index].gelesen = "Unread";
           } else {
-            document.querySelector(".lesestatus").innerHTML = "Read";
+            bookChange.innerHTML = "Read";
+            books[index].gelesen = "Read";
           }
         }
         targetElement = targetElement.parentElement;
       }
-
-      /*while (targetElement == null) {
-        //if (targetElement.matches(".read-book")) {
-        document.querySelector(".lesestatus").innerHTML = "Unread";
-        //}
-        targetElement = targetElement.parentElement;
-      }*/
     },
     true
   );
 };
-readChange(bookShelf, "click", true);
-element.removeEventListener("click", readChange, false);
+
+btn.addEventListener("click", addBookToLibary);
+addEventBook(bookShelf, "click");
